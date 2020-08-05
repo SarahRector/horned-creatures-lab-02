ef971fe2231853e1643a5f0402c46a9f3dad9f4c
SarahRector <rector.sarah.r@gmail.com>
2020-08-05 09:13:23 -0700

    fixed linting

--- src/ImageItem.js
+++ src/ImageItem.js
@@ -5,7 +5,7 @@ export default class ImageItem extends React.Component {
         return (
             <div>
                 <p className="title">{this.props.image.title}</p>
-                <img src={this.props.image.url.includes('modernfarmer') ? 'http://www.placekitten.com/200/200' : this.props.image.url}/>
+                <img src={this.props.image.url.includes('modernfarmer') ? 'http://www.placekitten.com/200/200' : this.props.image.url} alt={this.props.image.title}/>
                 <p>{this.props.image.description}</p>
             </div>
 
